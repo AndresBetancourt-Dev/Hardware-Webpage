6ac0c4613c728d39e2f8c99ce77d5792dfeac0b0
AndresBetancourt-Dev <contacto.betancourth@gmail.com>
2020-11-23 21:52:08 -0500

    🎞 Added Video, and Next.js Video import dependecy

--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,33 @@
-import React from "react";
+import { useEffect, useRef } from "react";
+import styles from "../styles/Home.module.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  const videoRef = useRef();
+
+  useEffect(() => {
+    setInterval(() => {
+      videoRef.current.play();
+    }, 1000);
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.tabs}></div>
+      <div className={styles.videoContainer}>
+        <video
+          ref={videoRef}
+          className={styles.video}
+          loop
+          autoPlay
+          muted
+          src={require("../static/videos/neon-lounge.mp4")}
+        />
+        <div className={styles.videoContent}>
+          <h1 className={styles.videoContentText}>Mantenimiento y Ensamble</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
