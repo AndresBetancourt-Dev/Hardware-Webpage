31cd151369abee432c644a7430af001bb705eb6a
AndresBetancourt-Dev <contacto.betancourth@gmail.com>
2020-11-24 19:43:19 -0500

    First Part of the Project v. 0.4

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
-import Home from "../pages/Home";
+import styles from "../styles/Home.module.css";
+import Layout from "../components/Layout";
+import HomeVideo from "../components/HomeVideo";
+import Head from "next/head";
+import HomeSubjects from "../components/HomeSubjects";
 
-export default function Index() {
-  return <Home />;
-}
+const Home = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>Home</title>
+        <meta
+          name="description"
+          content="Una página que ilustra todo lo visto en curso de Mantenimiento y Ensamble de Hardware"
+        />
+      </Head>
+      <div className={styles.Container}>
+        <HomeVideo />
+        <HomeSubjects />
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
